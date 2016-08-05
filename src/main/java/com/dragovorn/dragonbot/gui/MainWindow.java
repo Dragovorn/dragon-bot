feabc795a3b9f545f98e129fda155206a48bd3cc
Dragovorn <me@dragovorn.com>
2016-08-05 18:02:59 -0400

    Added main window class (Mainly for attempting to fix github)

---
+++ src/main/java/com/dragovorn/dragonbot/gui/MainWindow.java
@@ -0,0 +1,12 @@
+package com.dragovorn.dragonbot.gui;
+
+/**
+ * *************************************************************************
+ * (c) Dragovorn 2016. This file was created by Andrew at 6:02 PM.
+ * as of 8/5/16 the project dragonbot is Copyrighted.
+ * *************************************************************************
+ */
+public class MainWindow {
+
+    //TODO: Main Window
+}
