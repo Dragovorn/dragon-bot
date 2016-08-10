037095fda4b410bf9f53dfed8a7c89aac89a3818
Dragovorn <andrewburr011@gmail.com>
2016-08-10 09:20:15 -0400

    Finished the plugin loader
    Resolves #3

--- src/main/java/com/dragovorn/dragonbot/plugin/PluginLoader.java
+++ src/main/java/com/dragovorn/dragonbot/plugin/PluginLoader.java
@@ -1,5 +1,15 @@
 package com.dragovorn.dragonbot.plugin;
 
+import com.dragovorn.dragonbot.bot.Bot;
+import com.dragovorn.dragonbot.exceptions.InvalidPluginException;
+
+import java.io.File;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.Collections;
+import java.util.jar.JarFile;
+import java.util.zip.ZipEntry;
+
 /**
  * *************************************************************************
  * (c) Dragovorn 2016. This file was created by Andrew at 6:50 PM.
@@ -7,4 +17,60 @@ package com.dragovorn.dragonbot.plugin;
  * *************************************************************************
  */
 public class PluginLoader {
+
+    public BotPlugin loadPlugin(File file) throws InvalidPluginException {
+        if (file.getName().matches("(.+).(jar)$")) {
+            JarFile jar;
+
+            try {
+                jar = new JarFile(file);
+
+                URLClassLoader loader = new URLClassLoader(new URL[] {file.toURI().toURL()}, getClass().getClassLoader());
+
+                Plugin plugin = null;
+                BotPlugin botPlugin = null;
+
+                for (ZipEntry entry : Collections.list(jar.entries())) {
+                    if (entry.getName() != null && entry.getName().startsWith("__MACOSX")) {
+                        continue;
+                    }
+
+                    if (!entry.getName().matches("(.+).(class)$")) {
+                        continue;
+                    }
+
+                    Class<?> clazz = Class.forName(entry.getName().replace(".class", "").replace("/", "."), true, loader);
+
+                    Plugin pl = clazz.getAnnotation(Plugin.class);
+
+                    if (plugin != null && pl != null) {
+                        throw new InvalidPluginException("There are two classes with @Plugin annotations " + entry.getName());
+                    } else if (pl != null) {
+                        if (!BotPlugin.class.isAssignableFrom(clazz)) {
+                            throw new InvalidPluginException("The class with the @Plugin annotation does not extend BotPlugin " + entry.getName());
+                        }
+
+                        plugin = pl;
+                        botPlugin = (BotPlugin) clazz.newInstance();
+                    }
+                }
+
+                if (plugin == null) {
+                    throw new InvalidPluginException("There is no class with the @Plugin annotation " + file.getPath());
+                }
+
+                botPlugin.setInfo(new PluginInfo(plugin));
+
+                Bot.getInstance().getLogger().info("Loading " + plugin.name() + " v" + plugin.version() + "!");
+                botPlugin.onEnable();
+                Bot.getInstance().getLogger().info(plugin.name() + " v" + plugin.version() + " loaded!");
+
+                return botPlugin;
+            } catch (Exception exception) {
+                throw new InvalidPluginException("Encountered exception ", exception);
+            }
+        }
+
+        return null; // Shouldn't happen
+    }
 }
