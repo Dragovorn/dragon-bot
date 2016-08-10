b3f2913e68a840629e8dd53ee6dccc846d5fd60b
Dragovorn <me@dragovorn.com>
2016-08-10 09:20:15 -0400

    Finished the plugin loader
    Resolves #3

--- src/main/java/com/dragovorn/dragonbot/Core.java
+++ src/main/java/com/dragovorn/dragonbot/Core.java
@@ -1,13 +1,6 @@
 package com.dragovorn.dragonbot;
 
-import com.dragovorn.dragonbot.plugin.Plugin;
-
-import java.io.File;
-import java.net.URL;
-import java.net.URLClassLoader;
-import java.util.Collections;
-import java.util.jar.JarFile;
-import java.util.zip.ZipEntry;
+import com.dragovorn.dragonbot.bot.DragonBot;
 
 /**
  * *************************************************************************
@@ -17,43 +10,7 @@ import java.util.zip.ZipEntry;
  */
 public class Core {
 
-    private Core() {
-        for (File file : FileLocations.plugins.listFiles()) {
-            if (file.getName().matches("(.+).(jar)$")) {
-                JarFile jar;
-
-                try {
-                    jar = new JarFile(file);
-
-                    URLClassLoader loader = new URLClassLoader(new URL[] {file.toURI().toURL()}, getClass().getClassLoader());
-
-                    for (ZipEntry entry : Collections.list(jar.entries())) {
-                        if (entry.getName() != null && entry.getName().startsWith("__MACOSX")) {
-                            continue;
-                        }
-
-                        if (!entry.getName().matches("(.+).(class)$")) {
-                            continue;
-                        }
-
-                        Class<?> clazz = Class.forName(entry.getName().replace(".class", "").replace("/", "."), true, loader);
-
-                        Plugin plugin = clazz.getAnnotation(Plugin.class);
-
-                        if (plugin != null) {
-                            System.out.println("Name - " + plugin.name());
-                        }
-                    }
-                } catch (Exception exception) {
-
-                }
-            }
-        }
-    }
-
     public static void main(String[] args) throws Exception {
-//        new DragonBot();
-
-        new Core();
+        new DragonBot();
     }
 }
