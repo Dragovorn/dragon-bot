348aaf3cce7514753f5f033bbfa381e0600a4621
Dragovorn <me@dragovorn.com>
2016-08-09 20:02:46 -0400

    Worked on plugin loading re #3

---
+++ src/main/java/com/dragovorn/dragonbot/plugin/PluginInfo.java
@@ -0,0 +1,32 @@
+package com.dragovorn.dragonbot.plugin;
+
+/**
+ * *************************************************************************
+ * (c) Dragovorn 2016. This file was created by Andrew at 8:08 AM.
+ * as of 8/7/16 the project dragonbot is Copyrighted.
+ * *************************************************************************
+ */
+public class PluginInfo {
+
+    private final String name;
+    private final String version;
+    private final String author;
+
+    public PluginInfo(Plugin plugin) {
+        this.name = plugin.name();
+        this.version = plugin.version();
+        this.author = plugin.author();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
---
+++ src/main/java/com/dragovorn/dragonbot/plugin/PluginLoader.java
@@ -0,0 +1,10 @@
+package com.dragovorn.dragonbot.plugin;
+
+/**
+ * *************************************************************************
+ * (c) Dragovorn 2016. This file was created by Andrew at 6:50 PM.
+ * as of 8/7/16 the project dragonbot is Copyrighted.
+ * *************************************************************************
+ */
+public class PluginLoader {
+}
