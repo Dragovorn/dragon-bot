48037c1a214e8daefb14f5e5b9650952cd69178e
Dragovorn <me@dragovorn.com>
2016-08-13 16:14:03 -0400

    Began working on the auto updater re #7

---
+++ src/main/java/com/dragovorn/dragonbot/gui/panel/UpdatePanel.java
@@ -0,0 +1,18 @@
+package com.dragovorn.dragonbot.gui.panel;
+
+import com.amazonaws.services.s3.transfer.TransferManager;
+
+import javax.swing.*;
+
+/**
+ * *************************************************************************
+ * (c) Dragovorn 2016. This file was created by Andrew at 3:52 PM.
+ * as of 8/13/16 the project dragonbot is Copyrighted.
+ * *************************************************************************
+ */
+public class UpdatePanel extends JPanel {
+
+    public UpdatePanel(TransferManager manager) {
+        // TODO
+    }
+}
