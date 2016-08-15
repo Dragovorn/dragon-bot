c2c722a872a39d815db28c24527dfe16e5388c43
Dragovorn <andrewburr011@gmail.com>
2016-08-15 00:12:05 -0400

    Made a new module for the updater ref #15

---
+++ updater/src/main/java/com/dragovorn/dragonbot/updater/Updater.java
@@ -0,0 +1,18 @@
+package com.dragovorn.dragonbot.updater;
+
+/**
+ * *************************************************************************
+ * (c) Dragovorn 2016. This file was created by Andrew at 12:10 AM.
+ * as of 8/15/16 the project dragonbot is Copyrighted.
+ * *************************************************************************
+ */
+public class Updater {
+
+    Updater() {
+        // TODO: Updater GUI
+    }
+
+    public static void main(String[] args) {
+        // TODO: Updater
+    }
+}
