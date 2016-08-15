cfe39a9d3885bb18d243d7dc4a819490b315b0eb
Dragovorn <andrewburr011@gmail.com>
2016-08-14 20:10:54 -0400

    Auto-Update is now complete. Release 1.03 is now complete.
    Resolves #7

--- src/main/java/com/dragovorn/dragonbot/gui/panel/UpdatePanel.java
+++ src/main/java/com/dragovorn/dragonbot/gui/panel/UpdatePanel.java
@@ -1,9 +1,20 @@
 package com.dragovorn.dragonbot.gui.panel;
 
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.event.ProgressEvent;
+import com.amazonaws.event.ProgressListener;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.transfer.Download;
 import com.amazonaws.services.s3.transfer.TransferManager;
+import com.dragovorn.dragonbot.Core;
+import com.dragovorn.dragonbot.bot.Bot;
+import com.dragovorn.dragonbot.gui.MainWindow;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
+import java.net.URISyntaxException;
 
 /**
  * *************************************************************************
@@ -15,16 +26,73 @@ public class UpdatePanel extends JPanel {
 
     private JProgressBar progressBar;
 
+    private Download download;
+
+     private TransferManager manager;
+
     public UpdatePanel(TransferManager manager) {
+        this.manager = manager;
+
         progressBar = new JProgressBar(0, 100);
         progressBar.setStringPainted(true);
         progressBar.setString("Updating...");
 
-        Dimension size = new Dimension(200, 30);
-        add(progressBar);
+        JLabel label = new JLabel("Checking for updates...");
+
+        Dimension size = new Dimension(250, 30);
+        add(label);
         setSize(size);
         setPreferredSize(size);
         setMinimumSize(size);
         setMaximumSize(size);
+    }
+
+    public void update() {
+        S3Object object = manager.getAmazonS3Client().getObject(new GetObjectRequest("dl.dragovorn.com", "DragonBot/DragonBot.jar"));
+
+        try {
+            if (object.getObjectMetadata().getLastModified().getTime() > new File(Core.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath()).lastModified()) {
+                Bot.getInstance().getLogger().info("Found an update! Downloading now...");
+
+                removeAll();
+                add(progressBar);
+                MainWindow.getInstance().pack();
+
+                ProgressListener listener = (ProgressEvent event) -> {
+
+                    if (download == null) {
+                        return;
+                    }
+
+                    progressBar.setValue((int) download.getProgress().getPercentTransferred());
+
+                    switch (event.getEventType()) {
+                        case TRANSFER_COMPLETED_EVENT: {
+                            progressBar.setValue(100);
+                            Bot.getInstance().restart();
+                            break;
+                        } case TRANSFER_FAILED_EVENT: {
+                            try {
+                                AmazonClientException exception = download.waitForException();
+
+                                exception.printStackTrace();
+                            } catch(InterruptedException exception) { /* Won't happen */ }
+                            break;
+                        }
+                    }
+                };
+
+                new File(Core.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath()).delete();
+
+                GetObjectRequest request = new GetObjectRequest("dl.dragovorn.com", "DragonBot/DragonBot.jar").withGeneralProgressListener(listener);
+
+                download = manager.download(request, new File(Core.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath()));
+            } else {
+                Bot.getInstance().getLogger().info("No update found.");
+                MainWindow.getInstance().setContentPane(MainWindow.getInstance().getPanel());
+                MainWindow.getInstance().pack();
+                MainWindow.getInstance().center();
+            }
+        } catch (URISyntaxException exception) { /* Shouldn't toss an exception */ }
     }
 }
