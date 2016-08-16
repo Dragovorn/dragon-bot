95b31aae4f3cd6974a5f9fb18491f6a1894801a2
Dragovorn <me@dragovorn.com>
2016-08-16 16:35:43 -0400

    Began working on console commands, allowing plugins to add their own console commands.

---
+++ src/main/java/com/dragovorn/dragonbot/command/ConsoleCommand.java
@@ -0,0 +1,10 @@
+package com.dragovorn.dragonbot.command;
+
+/**
+ * *************************************************************************
+ * (c) Dragovorn 2016. This file was created by Andrew at 4:34 PM.
+ * as of 8/16/16 the project dragonbot is Copyrighted.
+ * *************************************************************************
+ */
+public abstract class ConsoleCommand {
+}
