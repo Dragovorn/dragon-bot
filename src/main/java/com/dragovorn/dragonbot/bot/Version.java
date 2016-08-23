8369bedde67c97a44dc907be690bc9d9f268ab88
Dragovorn <me@dragovorn.com>
2016-08-23 01:25:19 -0400

    Finished loads of HTTPClient things, began working more on twitch api integration.

--- src/main/java/com/dragovorn/dragonbot/bot/Version.java
+++ src/main/java/com/dragovorn/dragonbot/bot/Version.java
@@ -10,7 +10,7 @@ class Version {
 
     private static final char PATCH = 'a';
 
-    private static final int SNAPSHOT = 2;
+    private static final int SNAPSHOT = 3;
 
     private static final String VERSION = "1.04";
     static final String PRETTY_VERSION = VERSION + PATCH + (SNAPSHOT > 0 ? "_SNAPSHOT-" + SNAPSHOT : "");
