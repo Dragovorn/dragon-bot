6239c4383bf54d9730f46ee0ddac52ddae61640c
Dragovorn <me@dragovorn.com>
2016-08-25 22:21:58 -0400

    Release 1.04a Made the look and feel of the program to the system's look and feel, made a very minor change to the plugin annotation.

--- src/main/java/com/dragovorn/dragonbot/bot/Version.java
+++ src/main/java/com/dragovorn/dragonbot/bot/Version.java
@@ -10,7 +10,7 @@ class Version {
 
     private static final char PATCH = 'a';
 
-    private static final int SNAPSHOT = 6;
+    private static final int SNAPSHOT = 0;
 
     private static final String VERSION = "1.04";
     static final String PRETTY_VERSION = "v" + VERSION + PATCH + (SNAPSHOT > 0 ? "_SNAPSHOT-" + SNAPSHOT : "");
