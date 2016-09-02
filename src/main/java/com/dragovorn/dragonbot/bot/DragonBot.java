fb5a9484541182d8eb667785f93b47a1a124358f
Dragovorn <me@dragovorn.com>
2016-09-02 12:08:11 -0400

    changed the license to the MIT License

--- src/main/java/com/dragovorn/dragonbot/bot/DragonBot.java
+++ src/main/java/com/dragovorn/dragonbot/bot/DragonBot.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2016. Andrew Burr
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ *  associated documentation files (the "Software"), to deal in the Software without restriction,
+ *  including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ *  sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+ * THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE..
+ */
+
 package com.dragovorn.dragonbot.bot;
 
 import com.amazonaws.AmazonClientException;
@@ -46,12 +65,6 @@ import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- * *************************************************************************
- * (c) Dragovorn 2016. This file was created by Andrew at 3:27 PM.
- * as of 8/5/16 the project dragonbot is Copyrighted.
- * *************************************************************************
- */
 public class DragonBot extends Bot {
 
     private String name;
