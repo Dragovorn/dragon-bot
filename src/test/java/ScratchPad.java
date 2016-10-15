6fb730dce4afbced3fd486e3a06f68a6ba1f508a
Dragovorn <andrewburr011@gmail.com>
2016-10-15 08:57:00 -0400

    Mock up of update window.

--- src/test/java/ScratchPad.java
+++ src/test/java/ScratchPad.java
@@ -19,14 +19,83 @@
 
 import com.dragovorn.dragonbot.api.github.GitHubAPI;
 import com.github.rjeschke.txtmark.Processor;
-import org.junit.Test;
+
+import javax.swing.*;
+import javax.swing.text.*;
+import java.awt.*;
 
 public class ScratchPad {
 
-    @Test
-    public void scratchPad() throws Exception {
+    public static void main(String[] args) throws Exception {
         GitHubAPI api = new GitHubAPI("dragovorn", "dragon-bot-twitch", false);
 
-        System.out.println(Processor.process(api.getRelease("v1.05e").getString("body")));
+        JTextPane area = new JTextPane();
+        area.setEditorKit(new WrapEditorKit());
+        area.setSize(20, 20);
+        area.setContentType("text/html");
+        area.setEditable(false);
+        area.setText(Processor.process(api.getRelease("v1.05e").getString("body")));
+
+        JScrollPane scroll = new JScrollPane(area);
+        scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+
+        JButton button = new JButton("test");
+
+        JFrame frame = new JFrame("Update found!");
+        frame.setSize(500, 500);
+        frame.setResizable(false);
+        frame.setLayout(new FlowLayout());
+        frame.add(scroll);
+        frame.add(button);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+
+    static class WrapEditorKit extends StyledEditorKit {
+        ViewFactory defaultFactory=new WrapColumnFactory();
+        public ViewFactory getViewFactory() {
+            return defaultFactory;
+        }
+
+    }
+
+    static class WrapColumnFactory implements ViewFactory {
+        public View create(Element elem) {
+            String kind = elem.getName();
+            if (kind != null) {
+                if (kind.equals(AbstractDocument.ContentElementName)) {
+                    return new WrapLabelView(elem);
+                } else if (kind.equals(AbstractDocument.ParagraphElementName)) {
+                    return new ParagraphView(elem);
+                } else if (kind.equals(AbstractDocument.SectionElementName)) {
+                    return new BoxView(elem, View.Y_AXIS);
+                } else if (kind.equals(StyleConstants.ComponentElementName)) {
+                    return new ComponentView(elem);
+                } else if (kind.equals(StyleConstants.IconElementName)) {
+                    return new IconView(elem);
+                }
+            }
+
+            // default to text display
+            return new LabelView(elem);
+        }
+    }
+
+    static class WrapLabelView extends LabelView {
+        public WrapLabelView(Element elem) {
+            super(elem);
+        }
+
+        public float getMinimumSpan(int axis) {
+            switch (axis) {
+                case View.X_AXIS:
+                    return 0;
+                case View.Y_AXIS:
+                    return super.getMinimumSpan(axis);
+                default:
+                    throw new IllegalArgumentException("Invalid axis: " + axis);
+            }
+        }
+
     }
 }
