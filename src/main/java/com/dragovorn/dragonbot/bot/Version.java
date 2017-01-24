73e0e4f3a9679201389a7ca8d0a56c9884738c64
Dragovorn <me@dragovorn.com>
2017-01-24 14:24:56 -0500

    Finished off the portion of the twitch API I started but never finished

--- src/main/java/com/dragovorn/dragonbot/bot/Version.java
+++ src/main/java/com/dragovorn/dragonbot/bot/Version.java
@@ -21,7 +21,7 @@ package com.dragovorn.dragonbot.bot;
 
 class Version {
 
-    private static final char PATCH = 'b';
+    private static final char PATCH = 'c';
 
     private static final int SNAPSHOT = 0;
 
