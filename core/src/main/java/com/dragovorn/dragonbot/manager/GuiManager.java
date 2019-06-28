9f2414a5a3aaee464826e0af371492783097960a
Dragovorn <me@dragovorn.com>
2019-06-28 18:14:17 -0400

    Finish up scenes and rename DragonBotApplication to Main, because that's what it is now

--- core/src/main/java/com/dragovorn/dragonbot/manager/GuiManager.java
+++ core/src/main/java/com/dragovorn/dragonbot/manager/GuiManager.java
@@ -1,6 +1,6 @@
 package com.dragovorn.dragonbot.manager;
 
-import com.dragovorn.dragonbot.DragonBotApplication;
+import com.dragovorn.dragonbot.Main;
 import com.dragovorn.dragonbot.api.gui.scene.IScene;
 import com.dragovorn.dragonbot.gui.MainScene;
 import com.google.common.collect.Maps;
@@ -15,19 +15,48 @@ public final class GuiManager {
 
     private static Scene current;
 
-    private static final IScene DEFAULT = new MainScene();
+    private static final Map<String, IScene> scenes = Maps.newHashMap();
 
-    private static Map<String, IScene> scenes = Maps.newHashMap();
+    private static IScene DEFAULT;
 
     public static void init(Stage stage) {
         if (GuiManager.stage != null) {
             throw new IllegalStateException("GuiManager has already been initialized!");
         }
 
-        stage.setTitle("Dragon Bot v" + DragonBotApplication.VERSION);
-
         GuiManager.stage = stage;
+
+        DEFAULT = registerScene(new MainScene());
+
+        current = DEFAULT.toJFXScene();
+
+        stage.setScene(current);
+        stage.setTitle("Dragon Bot v" + Main.VERSION);
+//        stage.getIcons().add(FileSystem.getResource("our/icon")) TODO: Commission/create an icon for the bot.
+        stage.show();
     }
 
+    public void setScene(String key) {
+        setScene(scenes.get(key));
+    }
 
+    public void setScene(IScene scene) {
+        stage.setScene(scene.toJFXScene());
+
+    }
+
+    public static IScene registerScene(IScene scene) {
+        scenes.put(scene.getName(), scene);
+        scene.init();
+
+        return scene;
+    }
+
+    public static void setCurrentToDefault() {
+        current = DEFAULT.toJFXScene();
+    }
+
+    public static IScene getDefaultScene() {
+        return DEFAULT;
+    }
 }
