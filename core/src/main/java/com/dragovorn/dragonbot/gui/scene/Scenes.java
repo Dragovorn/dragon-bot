0b88057131d58b90225105b92b58355a70eb2ae7
Dragovorn <me@dragovorn.com>
2019-06-29 17:18:18 -0400

    Pull scenes out to an interface, and begin populating them

---
+++ core/src/main/java/com/dragovorn/dragonbot/gui/scene/Scenes.java
@@ -0,0 +1,14 @@
+package com.dragovorn.dragonbot.gui.scene;
+
+import com.dragovorn.dragonbot.DragonBot;
+import com.dragovorn.dragonbot.api.gui.scene.IScene;
+
+public interface Scenes {
+
+    IScene MAIN = register(new MainScene());
+    IScene ADVANCED_OPTIONS = register(new AdvancedScene());
+
+    static IScene register(IScene scene) {
+        return DragonBot.getInstance().getGuiManager().registerScene(scene);
+    }
+}
