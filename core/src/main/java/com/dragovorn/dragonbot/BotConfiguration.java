5b10135d3c8aa77db4689759d40d4125a82274ff
Dragovorn <me@dragovorn.com>
2019-07-01 01:13:56 -0400

    begin implementing configurations and fix a bug with them too

---
+++ core/src/main/java/com/dragovorn/dragonbot/BotConfiguration.java
@@ -0,0 +1,17 @@
+package com.dragovorn.dragonbot;
+
+import com.dragovorn.dragonbot.api.config.FileConfiguration;
+
+final class BotConfiguration extends FileConfiguration {
+
+    BotConfiguration() {
+        super(DragonBot.getInstance().getHomePath().resolve("config.json"));
+    }
+
+    @Override
+    public void addDefaults() {
+        this.defaults.put("console", false);
+        this.defaults.put("account.username", "");
+        this.defaults.put("account.oauth", "");
+    }
+}
